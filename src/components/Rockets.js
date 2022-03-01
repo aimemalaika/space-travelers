a2b3b1722b7201328322f9043d9bbd91eefb9b6c
marysol <marilirulita@gmail.com>
2022-03-01 15:54:22 -0800

    Add dispatch action to store filter data

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,15 +1,31 @@
+import { useDispatch } from "react-redux";
+import { getFetchData } from "../redux/rockets/rockets";
+
 const Rockets = () => {
+  const dispatch = useDispatch();
+
+  const storeData = (data) => {
+    const filterData = data.map((ele) => ({
+      id: ele.id,
+      rocket_name: ele.rocket_name,
+      description: ele.description,
+      flickr_images: ele.flickr_images,
+    }));
+
+    dispatch(getFetchData(filterData));
+  };
+
   const getData = async () => {
-    const answer = await fetch('https://api.spacexdata.com/v3/rockets');
+    const answer = await fetch("https://api.spacexdata.com/v3/rockets");
     const answerJason = await answer.json();
-    console.log(answerJason);
+    storeData(answerJason);
   };
 
   getData();
   return (
-  <div>
-    <h1>Rockets page</h1>
-  </div>
+    <div>
+      <h1>Rockets page</h1>
+    </div>
   );
 };
 
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,9 +1,21 @@
-const initialState = [];
+const initialState = {
+  rockets: [],
+};
+
+const FETCH_DATA = 'FETCH_DATA';
+
+export const getFetchData = (payload) => ({
+  type: FETCH_DATA,
+  payload,
+});
 
 const reducerRockets = (state = initialState, action) => {
   switch (action.type) {
-    case 'FIRST_ACTION':
-      return [];
+    case FETCH_DATA:
+      return {
+        ...state,
+        rockets: [...action.payload],
+      };
     case 'SECOND_ACTION':
       return [];
     default:
