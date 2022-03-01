4f982bf877a398e5897df1e563843b8b73a6b2c7
marysol <marilirulita@gmail.com>
2022-03-01 15:05:54 -0800

    Add get data async function

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,16 @@
-const Rockets = () => (
+const Rockets = () => {
+  const getData = async () => {
+    const answer = await fetch('https://api.spacexdata.com/v3/rockets');
+    const answerJason = await answer.json();
+    console.log(answerJason);
+  };
+
+  getData();
+  return (
   <div>
     <h1>Rockets page</h1>
   </div>
-);
+  );
+};
 
 export default Rockets;
