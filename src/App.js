8b11214be2d9ed05e12eb8bd2064b0374df70c2d
Aime Malaika <aimemalaika1995@gmail.com>
2022-03-02 01:05:02 +0200

    Created mission basic structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Missions from './components/Missions';
 import Rockets from './components/Rockets';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Rockets />} />
+        <Route path="/mission" element={<Missions />} />
       </Routes>
     </div>
   );
