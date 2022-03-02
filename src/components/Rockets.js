420d86799b340ddec88d0b0193972d615169b1fc
marysol <marilirulita@gmail.com>
2022-03-01 16:32:55 -0800

    Add thunk redux and get data actions

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,27 +1,14 @@
-import { useDispatch } from "react-redux";
-import { getFetchData } from "../redux/rockets/rockets";
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getData } from '../redux/rockets/rockets';
 
 const Rockets = () => {
   const dispatch = useDispatch();
 
-  const storeData = (data) => {
-    const filterData = data.map((ele) => ({
-      id: ele.id,
-      rocket_name: ele.rocket_name,
-      description: ele.description,
-      flickr_images: ele.flickr_images,
-    }));
+  useEffect(() => {
+    dispatch(getData());
+  }, []);
 
-    dispatch(getFetchData(filterData));
-  };
-
-  const getData = async () => {
-    const answer = await fetch("https://api.spacexdata.com/v3/rockets");
-    const answerJason = await answer.json();
-    storeData(answerJason);
-  };
-
-  getData();
   return (
     <div>
       <h1>Rockets page</h1>
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -4,10 +4,23 @@ const initialState = {
 
 const FETCH_DATA = 'FETCH_DATA';
 
-export const getFetchData = (payload) => ({
+const getFetchData = (payload) => ({
   type: FETCH_DATA,
   payload,
 });
+
+export const getData = () => async (dispatch) => {
+  const answer = await fetch('https://api.spacexdata.com/v3/rockets');
+  const answerJason = await answer.json();
+  const filterData = answerJason.map((ele) => ({
+    id: ele.id,
+    rocket_name: ele.rocket_name,
+    description: ele.description,
+    flickr_images: ele.flickr_images,
+  }));
+
+  dispatch(getFetchData(filterData));
+};
 
 const reducerRockets = (state = initialState, action) => {
   switch (action.type) {
