3e8004439b2e8887ceb9af15eef6cbc1094bfbd2
Aime Malaika <aimemalaika1995@gmail.com>
2022-03-02 02:34:16 +0200

    List space missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getMission } from '../redux/missions/mission';
 
-class Missions extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Mission</h1>
-      </div>
-    );
-  }
-}
+const Missions = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMission);
+  }, []);
+  const missions = useSelector((state) => state.missionReducer);
+  const missionTable = missions.map((mission) => (
+    <tr key={mission.mission_id}>
+      <td>{mission.mission_name}</td>
+      <td>{mission.description}</td>
+      <td className="text-center"><span className="status-field unavailable-ststus">NOT A MEMBER</span></td>
+      <td className="text-center"><button className="button-mission join-mission" type="button">JOIN MISSION</button></td>
+    </tr>
+  ));
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th className="mission-name">Mission</th>
+            <th className="mission-description">Description</th>
+            <th className="mission-status">Status</th>
+            <th aria-label="mission action" className="mission-action" />
+          </tr>
+        </thead>
+        <tbody>
+          { missionTable }
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Missions;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import reducerRockets from './rockets/rockets';
+import missionReducer from './missions/mission';
 
 const reducer = combineReducers({
   reducerRockets,
-  // additional reducers could be added here
+  missionReducer,
 });
 
 const store = createStore(
   reducer,
-  applyMiddleware(logger),
+  applyMiddleware(logger, thunk),
 );
 
 export default store;
